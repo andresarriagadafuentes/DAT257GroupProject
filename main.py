3f22e2556e98d9dcebb7ec57f91c46b6f3f3d756
andresarriagadafuentes <andres.arriagada@hotmail.com>
2023-04-11 14:36:43 +0200

    kgformula

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-hej
+def drinkingFormula():
+    weight = int(input("Please enter your weight in kg "))
+    print("You should drink this amount in dl " + str(weight * 0.5 * 0.29574*2.20462262185))
+
+
+if __name__ == "__main__":
+    drinkingFormula()
