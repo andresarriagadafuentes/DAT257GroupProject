bd1defbc7aad2d8e6d120f78fabf7b4d8efe826d
andresarriagadafuentes <andres.arriagada@hotmail.com>
2023-04-11 20:02:21 +0200

    added option for lbs or kg

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import math
+import string
+
+
 def drinkingFormula():
-    weight = int(input("Please enter your weight in kg "))
-    print("You should drink this amount in dl " + str(weight * 0.5 * 0.29574*2.20462262185))
+
+    kg_or_lbs = input("Do you want to use kg or lbs? ")
+
+    if kg_or_lbs == "kg":
+        weight = int(input("Please enter your weight in kg "))
+        print("You should drink a minimum of " + str(round((weight * 0.5 * 0.0295735296 * 2.20462262185),1)) + " liters of water to stay healthy")
+    elif kg_or_lbs == "lbs":
+        weight = int(input("Please enter your weight in lbs "))
+        print("You should drink a minimum of " + str(weight * 0.5) + " oz to stay healthy")
 
 
-def hej():
-    print("Drick vatten <3")
+
 
 
 if __name__ == '__main__':
-    hej()
     drinkingFormula()
 
