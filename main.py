d0afb741bc320043c56585f66bd89d5cdb996ad2
Mattias <matlin0902@gmail.com>
2023-04-17 14:50:06 +0200

    hej

--- main.py
+++ main.py
@@ -34,18 +34,6 @@ def drinkingFormula1(weight):
     return water
 
 
-def drinkingFormula():
-
-    kg_or_lbs = input("Do you want to use kg or lbs? ")
-
-    if kg_or_lbs == "kg":
-        weight = int(input("Please enter your weight in kg "))
-        print("You should drink a minimum of " + str(round((weight * 0.5 * 0.0295735296 * 2.20462262185),1)) + " liters of water to stay healthy")
-    elif kg_or_lbs == "lbs":
-        weight = int(input("Please enter your weight in lbs "))
-        print("You should drink a minimum of " + str(weight * 0.5 * 2.2046) + " oz to stay healthy")
-
-
 
 
 
