93053e6b07569560da2f63f4f3bbc73125e50e95
Isak Borovac <isak.borovac@gmail.com>
2023-05-02 08:41:03 +0200

    Början på session

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import string
 from datetime import timedelta
 
 from flask import Flask, redirect, url_for, render_template, request,session
-from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
+
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = "AyyLMAO"
-app.permanent_session_lifetime = timedelta(minutes=5)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#app.secret_key = "AyyLMAO"
+#app.permanent_session_lifetime = timedelta(minutes=5)
 
 '''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
