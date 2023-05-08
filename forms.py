8c86716576b608547af807b20e79f9a15bc8f283
Mattias <matlin0902@gmail.com>
2023-05-08 13:22:08 +0200

    hej

--- forms.py
+++ forms.py
@@ -22,14 +22,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-        username = StringField('Username',
-                               validators=[DataRequired(), Length(min=2, max=20)])
-        lbs_or_kg = StringField('lbs_or_kg',validators=[DataRequired(), Length(min=2, max=3)])
-
-        weight = IntegerField('Weight',
-                              validators=[DataRequired()]
-                              )
-
+        username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
         password = PasswordField('Password', validators=[DataRequired()])
         remember = BooleanField('Remember me')
         submit = SubmitField('Login')
