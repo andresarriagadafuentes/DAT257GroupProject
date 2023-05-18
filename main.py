580995d55ad1c0d60c2f8174ce21c4f5db4e2edc
Mattias <matlin0902@gmail.com>
2023-05-18 14:39:48 +0200

    hejje

--- main.py
+++ main.py
@@ -48,8 +48,11 @@ def calculator():
         intake = drinkingFormula1(form.lbs_or_kg.data,form.weight.data,form.minutes_of_exercise.data)
         user = flask_login.current_user
         hist = History(user=user.username, waterGoal=intake, waterIntake=random.uniform(intake-1,intake+1), date= db.func.current_date())
-        db.session.add(hist)
-        db.session.commit()
+        try: 
+            db.session.add(hist)
+            db.session.commit()
+        except:
+            db.session.commit()
         return redirect(url_for("yourwaterintake"))
 
     else:
